c72efce1154ce86bd849382decafa4ab33bb0d0e
Thiago Raia de Moura <thiagoraia2004@gmail.com>
2024-08-31 10:35:23 -0300

    adjusting server.ts to use routes

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,12 @@
 import express from 'express'
 import dataBase from './database/ormconfig'
+import routes from './routes'
 
 const app = express()
 const port = process.env.PORT || 3000
+
+app.use(express.json()) // habilita o express para receber dados no formato json
+app.use(routes) // habilita as rotas
 
 app.listen(port, () => {
   console.log(`Servidor executando na porta ${port}`)
