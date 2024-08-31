6dfe764dd6124b6e438429f626157234d474d37e
Thiago Raia de Moura <thiagoraia2004@gmail.com>
2024-08-31 11:46:24 -0300

    add cors, adjust server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,14 @@
 import express from 'express'
 import dataBase from './database/ormconfig'
 import routes from './routes'
+import cors from 'cors'
 
 const app = express()
 const port = process.env.PORT || 3000
 
 app.use(express.json()) // habilita o express para receber dados no formato json
 app.use(routes) // habilita as rotas
+app.use(cors())
 
 app.listen(port, () => {
   console.log(`Servidor executando na porta ${port}`)
