1d23a28503aa957992e52c743a4fd13cda58364b
Thiago Raia de Moura <thiagoraia2004@gmail.com>
2024-08-31 10:19:23 -0300

    ormconfig

---
+++ src/database/ormconfig.ts
@@ -0,0 +1,21 @@
+import { DataSource } from 'typeorm'
+
+const dataBase = new DataSource({
+  type: 'sqlite',
+  database: process.env.DATABASE || './src/database/database.sqlite',
+  entities: [
+    './src/models/*.ts'
+  ],
+  logging: true, // log das queries executadas
+  synchronize: true // cria as tabelas automaticamente
+})
+
+dataBase.initialize()
+  .then(() => {
+    console.log(`Banco de dados inicializado`);
+  })
+  .catch((err) => {
+    console.error(`Erro ao inicializar o banco de dados`, err);
+  })
+
+export default dataBase
