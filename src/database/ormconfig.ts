60d206e217757501104fcdfe05bfc6e869e7e778
Thiago Raia de Moura <thiagoraia2004@gmail.com>
2024-08-31 11:39:24 -0300

    adjust ormconfig to deploy

--- src/database/ormconfig.ts
+++ src/database/ormconfig.ts
@@ -1,13 +1,14 @@
+import { join } from 'path';
 import { DataSource } from 'typeorm'
 
 const dataBase = new DataSource({
-  type: 'sqlite',
-  database: process.env.DATABASE || './src/database/database.sqlite',
-  entities: [
-    './src/models/*.ts'
-  ],
-  logging: true, // log das queries executadas
-  synchronize: true // cria as tabelas automaticamente
+    type: 'sqlite',
+    database: process.env.DATABASE || './src/database/database.sqlite',
+    entities: [
+      join(__dirname, '..', 'models/*.{ts,js}')
+    ],
+    logging: true,
+    synchronize: true
 })
 
 dataBase.initialize()
