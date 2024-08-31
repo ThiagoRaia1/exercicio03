867f903406a4588f23635beca487daba3276f7db
Thiago Raia de Moura <thiagoraia2004@gmail.com>
2024-08-31 10:39:39 -0300

    creating task to list all tasks

--- src/controllers/task/task.controller.ts
+++ src/controllers/task/task.controller.ts
@@ -2,18 +2,24 @@ import { Request, Response } from 'express'
 import Task from '../../models/task.entity'
 
 export default class TaskController {
-  static async store (req: Request, res: Response) {
-    const { title, completed } = req.body
 
-    if (!title) {
-      return res.status(400).json({ error: 'O título é obrigatório' })
+    static async index(req: Request, res: Response) {
+        const tasks = await Task.find()
+        return res.json(tasks)
     }
 
-    const task = new Task()
-    task.title = title
-    task.completed = completed || false
-    await task.save()
+    static async store(req: Request, res: Response) {
+        const { title, completed } = req.body
 
-    return res.status(201).json(task)
-  }
+        if (!title) {
+            return res.status(400).json({ error: 'O título é obrigatório' })
+        }
+
+        const task = new Task()
+        task.title = title
+        task.completed = completed || false
+        await task.save()
+
+        return res.status(201).json(task)
+    }
 }
--- src/routes/task/task.routes.ts
+++ src/routes/task/task.routes.ts
@@ -2,7 +2,7 @@ import { Router } from 'express'
 import TaskController from '../../controllers/task/task.controller'
 
 const taskRoutes = Router()
-
+taskRoutes.get('/', TaskController.index)
 taskRoutes.post('/', TaskController.store)
 
 export default taskRoutes
